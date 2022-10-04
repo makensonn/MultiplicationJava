73e36e68554b12b5804adba578c9ee07c13cf6c8
Makenson Noel <Makenson.noel@yahoo.com>
2022-10-04 09:18:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
-
+class Main {
+  public static void main(String[] args) {
+    int number1,number2;
+    number1=1;
+    while(number1<=12){
+      number2=1;
+      while(number2<=12){
+        
+        System.out.println(number1+" x "+number2+" = "+number1*number2);
+        number2++;
+      }
+      number1++;
+    }
+  }
+}
